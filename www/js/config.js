958b0cebe5728b3b28a239cff3a7fe569e03d4d6
Nate Ross <nate.ross@alliedpayment.com>
2017-10-09 10:51:19 -0400

    make iframe full screen

--- www/js/config.js
+++ www/js/config.js
@@ -2,10 +2,10 @@
 // remove '.demo' from the origin & api urls
 //
 var Config = {
-  privateKey: '<TODO_REPLACE_WITH_PRIVATE_KEY>',
-  publicKey: '<TODO_REPLACE_WITH_PUBLIC_KEY>',
-  domain: '<TODO_REPLACE_WITH_DOMAIN>',
-  origin: 'https://<TODO_REPLACE_WITH_DOMAIN>.alliedpayment.com',
+  privateKey: 'KcFlXjoOb5odaegKytM1LCsRwgw8Rpd1ds24NhNj',
+  publicKey: 'WSIlIrP6W2XY70eqkppk',
+  domain: 'ALLIED',
+  origin: 'https://billpay.demo.alliedpayment.com',
   ssoRequest: 'js/ssoRequest.json',
   apiUrl: 'https://api.demo.alliedpayment.com'
 };
